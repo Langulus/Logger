c4c967c597f15cdafd94281b9a1ba9f07857e543
Dimo Markov <dimomarkov@gmail.com>
2023-04-26 11:06:21 +0300

    Fixed test

--- test/Main.hpp
+++ test/Main.hpp
@@ -6,7 +6,7 @@
 /// See LICENSE file, or https://www.gnu.org/licenses									
 ///																									
 #pragma once
-#include <LangulusLogger.hpp>
+#include <Logger/Logger.hpp>
 #include <cstdint>
 #include <cstddef>
 #include <vector>
