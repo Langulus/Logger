0d1d5d22e54f9ef8b371975f43638a35fe23c301
Epixu <dimomarkov@gmail.com>
2025-02-16 19:35:18 +0200

    Using the common testing library

--- include/Langulus/Logger/Logger.hpp
+++
@@ -1,9 +0,0 @@
-///                                                                           
-/// Langulus::Logger                                                          
-/// Copyright (c) 2012 Dimo Markov <team@langulus.com>                        
-/// Part of the Langulus framework, see https://langulus.com                  
-///                                                                           
-/// SPDX-License-Identifier: MIT                                              
-///                                                                           
-#pragma once
-#include "../../../source/Logger.hpp"
